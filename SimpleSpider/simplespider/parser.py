723c47e86f965f1069a5023d126987445ebc1e5c
CarySun <carysun@outlook.com>
2019-08-30 13:06:48 +0800

    qiancheng

--- SimpleSpider/simplespider/parser.py
+++ SimpleSpider/simplespider/parser.py
@@ -16,13 +16,12 @@ from bloom_filter import BloomFilter
 
 class WebParser(object):
     def __init__(self):
-        self.bloom_filter = BloomFilter(redis.StrictRedis(host='localhost', port=6379), 'job_url')
+        self.bloom_filter = BloomFilter(redis.StrictRedis(host='localhost', port=6379), 'job_url_et')
 
-        #self.selector = Selector()
 
-    def list_zhilian(self, response_text):
+    def list_zhilian(self, response):
         urls = []
-        page = json.loads(response_text)
+        page = json.loads(response.text)
 
         if not len(page['data']['results']):
             return None
@@ -35,8 +34,17 @@ class WebParser(object):
         return urls
 
 
-    def list_qiancheng(self):
-        pass
+    def list_qiancheng(self, response):
+        urls = []
+        response.encoding=response.apparent_encoding
+        selector = Selector(response, type='html')
+        for url in selector.xpath('//div[@class="el"]/p/span/a/@href').getall():
+            if not self.bloom_filter.exists(url):
+                self.bloom_filter.insert(url)
+                urls.append(url)
+        print('done one page')
+        return urls
+
 
     def list_boss(self):
         pass
@@ -48,26 +56,45 @@ class WebParser(object):
         pass
 
     def content_zhilian(self, response, database):
-        pp = Selector(response)
-        title = pp.xpath('//*[@class="summary-plane__title"]/text()').extract_first()
-        salary = pp.xpath('//*[@class="summary-plane__salary"]/text()').extract_first()
-        city = pp.xpath('//*[@class="summary-plane__info"]/li/a/text()').extract_first()
+        response.encoding = response.apparent_encoding
+        selector = Selector(response)
+        title = selector.xpath('//*[@class="summary-plane__title"]/text()').extract_first()
+        salary = selector.xpath('//*[@class="summary-plane__salary"]/text()').extract_first()
+        city = selector.xpath('//*[@class="summary-plane__info"]/li/a/text()').extract_first()
         # description 结构上比较混乱,先爬取再说
-        description = pp.xpath('//*[@class="describtion__detail-content"]').extract()
-        #description = ''.join(description).replace("<p>", "").replace("<br>", "").replace("<b>", "").replace("</b>", "").replace("</p>", "\n")
-        summary_info = pp.xpath('//*[@class="summary-plane__info"]/li/text()').extract()
+        description = selector.xpath('//*[@class="describtion__detail-content"]').extract()
+        summary_info = selector.xpath('//*[@class="summary-plane__info"]/li/text()').extract()
 
         if len(summary_info) == 3:
-            experience = pp.xpath('//*[@class="summary-plane__info"]/li/text()').extract()[0]
-            education = pp.xpath('//*[@class="summary-plane__info"]/li/text()').extract()[1]
+            experience = selector.xpath('//*[@class="summary-plane__info"]/li/text()').extract()[0]
+            education = selector.xpath('//*[@class="summary-plane__info"]/li/text()').extract()[1]
             data = {"title":title, "salary":salary, "city":city, "experience":experience, "education":education, "description":description}
         else:
             data = {"title":title, "salary":salary, "city":city, "summary_info":summary_info, "description":description}
 
         database.insert(data)
 
-    def content_qiancheng(self):
-        pass
+    def content_qiancheng(self, response, database):
+        response.encoding = response.apparent_encoding
+        selector = Selector(response)
+        title = selector.xpath('//div[@class="cn"]/h1/text()').extract_first()
+        salary = selector.xpath('//div[@class="cn"]/strong/text()').extract_first()
+
+        # description 结构上比较混乱,先爬取再说
+        description = selector.xpath('//div[@class="bmsg job_msg inbox"]/*[not(@class)]').getall()
+        summary_info = city = selector.xpath('//p[@class="msg ltype"]/@title').extract_first().split('\xa0\xa0|\xa0\xa0')
+
+        if len(summary_info) >= 3:
+            city = summary_info[0]
+            experience = summary_info[1]
+            education = summary_info[2]
+            data = {"title":title, "salary":salary, "city":city, "experience":experience, "education":education, "description":description}
+        else:
+            data = {"title":title, "salary":salary, "summary_info":summary_info, "description":description}
+
+        database.insert(data)
+
+
     def content_boss(self):
         pass
     def content_shixi(self):
--- SimpleSpider/simplespider/spider.py
+++ SimpleSpider/simplespider/spider.py
@@ -32,19 +32,59 @@ class JobSpider(object):
     def __init__(self):
         self.bloom_filter = BloomFilter(redis.StrictRedis(host='localhost', port=6379), 'job_url')
         self.parser = WebParser()
-        self.keywords = {'机器学习':'machine_learning', '数据挖掘':'data_mining', '知识图谱': 'knowledge_graph', '推荐系统':'recommended_system', '深度学习':'deep_learninig', '算法工程师':'algorithm_engineer', '前端':'front_end', '后端':'rear_end', 'Java':'java', 'Python':'python', '产品经理':'product_manager', '数字产品经理':'digital_product_manager'}
+        self.keywords = {'机器学习':'machine_learning', '数据挖掘':'data_mining', '知识图谱': 'knowledge_graph', '推荐系统':'recommended_system', '深度学习':'deep_learninig'
+            , '算法工程师':'algorithm_engineer', '前端':'front_end', '后端':'rear_end', 'Java':'java', 'Python':'python', '产品经理':'product_manager', '数字产品经理':'digital_product_manager'}
+
+    def _ui(self):
+        pass
 
     def crawl_zhilian(self, city, keyword):
         url_list = []  # todo url_list 做成堆栈形式
-        begin_url = 'https://fe-api.zhaopin.com/c/i/sou?start={0}&pageSize=90&cityId={1}&salary=0,0&workExperience=-1&education=-1&companyType=-1&employmentType=-1&jobWelfareTag=-1&kw={2}&kt=3'
+        begin_url = 'https://fe-api.zhaopin.com/c/i/sou?start={page}&pageSize=90&cityId={city}&salary=0,0&workExperience=-1&education=-1&companyType=-1&employmentType=-1&jobWelfareTag=-1&kw={keyword}&kt=3'
         database = MongoDB('zhilian', self.keywords[keyword])
 
+        self._get_list(begin_url, city, keyword, url_list,page_weight=90)
+
+        print(keyword , city ,'list parser done!')
+        print(len(url_list))
+
+        self._get_content(database, url_list)
+
+    def _get_content(self, database, url_list, web_name=None):
+        # todo 此处改为多线程
+        if url_list:
+            for url in url_list:
+                try:
+                    response = requests.get(url, headers=headers)
+                    if response.status_code != 200:
+                        print('anti-spider in content: ', response.status_code)
+                        print('error url:', url)
+                        # todo 反爬虫代理函数
+                        time.sleep(3)
+                        response = requests.get(url, headers=headers)
+                        if response.status_code != 200:
+                            print('give up:', url)
+                        else:
+                            if web_name == 'zhilian':
+                                self.parser.content_zhilian(response, database)
+                            if web_name == 'qiancheng':
+                                self.parser.content_qiancheng(response, database)
+                        continue
+                    if web_name == 'zhilian':
+                        self.parser.content_zhilian(response, database)
+                    if web_name == 'qiancheng':
+                        self.parser.content_qiancheng(response, database)
+                except Exception as e:
+                    print('request_job_contain error : {}'.format(e))
+
+    def _get_list(self, begin_url, city, keyword, url_list, page_weight=1, page_begin=0, web_name=None):
         for page in range(1000):
             try:
-                response = requests.get(begin_url.format(page*90, city, keyword), headers=headers)
+                p = begin_url.format(page=page * page_weight + page_begin, city=city, keyword=keyword)
+                response = requests.get(begin_url.format(page=page * page_weight + page_begin, city=city, keyword=keyword), headers=headers)
                 if response.status_code != 200:
                     print('anti-spider')
-                    continue # 如果用下面的话,记得去掉这个return
+                    continue  # 如果用下面的话,记得去掉这个return
                     # # todo 反爬虫代理函数
                     # proxy = get_proxy.get_proxy()
                     # if not proxy:
@@ -61,40 +101,28 @@ class JobSpider(object):
                     #     print("Can't seek useful proxy!")
                     #     return
                 else:
-                    urls = self.parser.list_zhilian(response.text)
+                    if web_name == 'zhilian':
+                        urls = self.parser.list_zhilian(response)
+                    if web_name == 'qiancheng':
+                        urls = self.parser.list_qiancheng(response)
+
                 if urls is None:
                     break
                 url_list.extend(urls)
             except Exception as e:
                 print('request_job_list error : {}'.format(e))
 
-        print(keyword , city ,'list parser done!')
+    def crawl_qiancheng(self, city, keyword):
+        url_list = []
+        begin_url ='https://search.51job.com/list/{city},000000,0000,00,9,99,{keyword},2,{page}.html?lang=c&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&ord_field=0&dibiaoid=0&line=&welfare='
+        database = MongoDB('qiancheng', self.keywords[keyword])
+
+        self._get_list(begin_url, city, keyword, url_list, page_begin=1, web_name='qiancheng')
+
+        print(keyword, city, 'list parser done!')
         print(len(url_list))
 
-        # todo 此处改为多线程
-        if url_list:
-            for url in url_list:
-                try:
-                    response = requests.get(url, headers=headers)
-                    if response.status_code != 200:
-                        print('anti-spider in content: ', response.status_code)
-                        print('error url:', url)
-                        # todo 反爬虫代理函数
-                        time.sleep(3)
-                        response = requests.get(url, headers=headers)
-                        if response.status_code != 200:
-                            print('give up:',url)
-                        else:
-                            self.parser.content_zhilian(response, database)
-                        continue
-                    self.parser.content_zhilian(response, database)
-                except Exception as e:
-                    print('request_job_contain error : {}'.format(e))
-
-
-
-    def crawl_qiancheng(self):
-        pass
+        self._get_content(database, url_list, web_name='qiancheng')
 
     def crawl_boss(self):
         pass
@@ -125,12 +153,14 @@ class JobSpider(object):
 
 if __name__ == '__main__':
     test = JobSpider()
-    cities = {'北京': '530', '深圳': '765', '上海': '538', '广州': '763', '成都': '801', '西安': '854'}
+    #cities = {'北京': '530', '深圳': '765', '上海': '538', '广州': '763', '成都': '801', '西安': '854', '重庆': '551'}
 
+    cities = {'北京': '010000', '深圳': '040000', '上海': '020000', '广州': '030200', '成都': '090200', '西安': '200200',
+             '重庆': '060000'}
     for keyword in keywords:
 
         for city in cities.values():
-            test.crawl_zhilian(city, keyword)
+            test.crawl_qiancheng(city, keyword)
 
         print(keyword, ' done!')
         time.sleep(60)
